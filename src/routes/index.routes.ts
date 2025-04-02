e72acab24ab59b7b8969fae645c397687fb5f2d4
KristinFrida <kfs14@hi.is>
2025-04-02 13:17:37 +0000

    prófum þetta

---
+++ src/routes/index.routes.ts
@@ -0,0 +1,37 @@
+import { Hono } from 'hono'
+import { userRoutes } from './users.js'
+import { salaryRoutes } from './salaries.js'
+
+export const api = new Hono()
+
+const routes = [
+  {
+    href: '/',
+    methods: ['GET'],
+  },
+  {
+    href: '/users',
+    methods: ['GET', 'POST'],
+    description: 'Skrá nýjan notanda og sækja alla notendur með launaupplýsingum',
+  },
+  {
+    href: '/users/:id',
+    methods: ['GET', 'PATCH', 'DELETE'],
+    description: 'Sækja, breyta eða eyða notanda og tengdum launum',
+  },
+  {
+    href: '/salaries',
+    methods: ['POST'],
+    description: 'Bæta við launatöflu fyrir notanda',
+  },
+  {
+    href: '/salaries/:id',
+    methods: ['PATCH', 'DELETE'],
+    description: 'Uppfæra eða eyða launatöflu',
+  }
+]
+
+api.get('/', (c) => c.json(routes))
+
+api.route('/users', userRoutes)
+api.route('/salaries', salaryRoutes)
